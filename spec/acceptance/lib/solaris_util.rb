b01da59d269fad6ebaf735fa92a8bbac2f10df53
Gabriel Nagy <gabriel.nagy@puppet.com>
2021-10-01 14:08:07 +0300

    (MODULES-11197) Update to pdk-templates 2.2.0
    Includes REFERENCE.md ToC fixes for MODULES-8183.

--- spec/acceptance/lib/solaris_util.rb
+++ spec/acceptance/lib/solaris_util.rb
@@ -2,28 +2,28 @@ OPTS = { poolpath: '/ztstpool', pool: 'tstpool', fs: 'tstfs' }.freeze
 
 def zfs_clean(agent, o = {})
   o = OPTS.merge(o)
-  on agent, 'zfs destroy -f -r %s/%s ||:' % [o[:pool], o[:fs]]
-  on agent, 'zpool destroy -f %s ||:' % o[:pool]
-  on agent, 'rm -rf %s ||:' % o[:poolpath]
+  on agent, "zfs destroy -f -r #{o[:pool]}/#{o[:fs]} ||:"
+  on agent, "zpool destroy -f #{o[:pool]} ||:"
+  on agent, "rm -rf #{o[:poolpath]} ||:"
 end
 
 def zfs_setup(agent, o = {})
   o = OPTS.merge(o)
-  on agent, 'mkdir -p %s/mnt' % o[:poolpath]
-  on agent, 'mkdir -p %s/mnt2' % o[:poolpath]
-  on agent, 'mkfile 64m %s/dsk' % o[:poolpath]
-  on agent, 'zpool create %s %s/dsk' % [o[:pool], o[:poolpath]]
+  on agent, "mkdir -p #{o[:poolpath]}/mnt"
+  on agent, "mkdir -p #{o[:poolpath]}/mnt2"
+  on agent, "mkfile 64m #{o[:poolpath]}/dsk"
+  on agent, "zpool create #{o[:pool]} #{o[:poolpath]}/dsk"
 end
 
 def zpool_clean(agent, o = {})
   o = OPTS.merge(o)
-  on agent, 'zpool destroy -f %s ||:' % o[:pool]
-  on agent, 'rm -rf %s ||:' % o[:poolpath]
+  on agent, "zpool destroy -f #{o[:pool]} ||:"
+  on agent, "rm -rf #{o[:poolpath]} ||:"
 end
 
 def zpool_setup(agent, o = {})
   o = OPTS.merge(o)
-  on agent, 'mkdir -p %s/mnt||:' % o[:poolpath]
-  on agent, 'mkfile 100m %s/dsk1 %s/dsk2 %s/dsk3 %s/dsk5 ||:' % ([o[:poolpath]] * 4)
-  on agent, 'mkfile 50m %s/dsk4 ||:' % o[:poolpath]
+  on agent, "mkdir -p #{o[:poolpath]}/mnt||:"
+  on agent, "mkfile 100m #{o[:poolpath]}/dsk1 #{o[:poolpath]}/dsk2 #{o[:poolpath]}/dsk3 #{o[:poolpath]}/dsk5 ||:"
+  on agent, "mkfile 50m #{o[:poolpath]}/dsk4 ||:"
 end
